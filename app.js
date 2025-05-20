904359763eca23e4f1e95b1e2f50bd59845baede
Suhani Singh <singhsuhani1610@gmail.com>
2025-05-20 07:42:11 +0530

    New Changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
+
+require('./init/index.js')
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
